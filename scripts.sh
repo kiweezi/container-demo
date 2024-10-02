e7494aecf16d9958fc8a0029d933ef7f11074a7f
kiweezi <46600706+kiweezi@users.noreply.github.com>
2024-10-02 09:17:11 +0100

    enhance script

--- scripts.sh
+++ scripts.sh
@@ -1,3 +1,7 @@
+### Setup overview ###
+# Docker info.
+docker --version
+
 ### Building images ###
 # Build the image.
 docker build -t container-demo -f Dockerfile .
@@ -12,17 +16,20 @@ docker run --publish 8080:8080 container-demo
 curl http://localhost:8080/api/
 
 ### Interacting with the registry ###
+# Tag the image with metadata for the registry.
+docker tag container-demo mytmptst.azurecr.io/samples/container-demo
 # Push image to the registry.
 docker push mytmptst.azurecr.io/samples/container-demo
 
 # Remove image.
 docker image remove container-demo
+docker image remove mytmptst.azurecr.io/samples/container-demo --force
 
 # Pull image from the registry.
 docker pull mytmptst.azurecr.io/samples/container-demo
 
 # Run the container.
-docker run --publish 8080:8080 container-demo
+docker run --publish 8080:8080 mytmptst.azurecr.io/samples/container-demo
 
 # Hit the default endpoint.
 curl http://localhost:8080/api/
